f2ba8034e17546e365ce034eb37b4e8d2533ad4b
Mariell Hoversholm <proximyst@proximyst.com>
2021-02-14 16:06:22 +0100

    P023

--- src/bin/p012.rs
+++ src/bin/p012.rs
@@ -29,18 +29,6 @@ impl Iterator for TriangleNumbers {
 }
 
 #[test]
-fn test_raw_numbers() {
-    // This algorithm doesn't work on 1.
-    // assert_eq!(1, count_divisors(&1));
-    assert_eq!(2, count_divisors(&3));
-    assert_eq!(4, count_divisors(&6));
-    assert_eq!(4, count_divisors(&10));
-    assert_eq!(4, count_divisors(&15));
-    assert_eq!(4, count_divisors(&21));
-    assert_eq!(6, count_divisors(&28));
-}
-
-#[test]
 fn test_triangles() {
     use itertools::Itertools as _;
     assert_eq!(
---
+++ src/bin/p023.rs
@@ -0,0 +1,52 @@
+use num::BigUint;
+use once_cell::sync::Lazy;
+use rayon::prelude::*;
+use std::collections::HashSet;
+
+static ABUNDANT_NUMBERS: Lazy<HashSet<u64>> = Lazy::new(|| {
+    // We know abundant numbers exist in [12, 28123)
+    (12u64..28123)
+        .into_par_iter()
+        .filter(|&n| is_abundant(n))
+        .collect()
+});
+
+fn main() {
+    println!(
+        "{}",
+        // [28123,->) are all sums
+        (1u64..28123)
+            .into_par_iter()
+            .filter(|&i| !is_abundant_sum(i))
+            .sum::<u64>()
+    );
+}
+
+fn sum_divisors(u: u64) -> u64 {
+    (1..u / 2 + 1).into_par_iter().filter(|n| u % n == 0).sum::<u64>()
+}
+
+fn is_abundant(n: u64) -> bool {
+    let divisors = BigUint::from(sum_divisors(n));
+    divisors > BigUint::from(n)
+}
+
+fn is_abundant_sum(n: u64) -> bool {
+    (1..=n / 2 + 1)
+        .into_par_iter()
+        .filter(|i| ABUNDANT_NUMBERS.contains(i))
+        .any(|i| is_abundant(n - i))
+}
+
+#[test]
+fn test() {
+    for i in 1..12 {
+        assert!(!is_abundant(i));
+    }
+    assert!(is_abundant(12));
+
+    assert!(is_abundant_sum(24));
+    for i in 28123..28133 {
+        assert!(is_abundant_sum(i));
+    }
+}
